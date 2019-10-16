838e405a03158ce2f6a3ec7f2a48bdbac0a0fdd2
Alex Merry <45360044+Kalungo@users.noreply.github.com>
2019-10-16 12:14:24 +0100

    Add files via upload

--- Kal.js
+++ Kal.js
@@ -1,13 +1,11 @@
 let w = 0, h = 0;
 const image = new Image();
-const opp = 242, adj = 420, hyp = 480;
-const triangleCanvas = new OffscreenCanvas(opp, adj);
-const hexagonCanvas = new OffscreenCanvas(2*hyp, 2*adj);
-
+let i=0
+let name=false;
 function fixSize() {
     w = window.innerWidth;
     h = window.innerHeight;
-    const canvas = document.getElementById('rotateyCanvas');
+    const canvas = document.getElementById('kaleidoscopeCanvas');
     canvas.width = w;
     canvas.height = h;
 }
@@ -17,10 +15,15 @@ function pageLoad() {
     window.addEventListener("resize", fixSize);
     fixSize();
 
-    image.src = "maxresdefault.jpg";
+    image.src = 'maxresdefault.jpg';
     image.onload = () => window.requestAnimationFrame(redraw);
 
 }
+
+const opp = 240, adj = 416, hyp = 480;
+
+const triangleCanvas = new OffscreenCanvas(opp, adj);
+const hexagonCanvas = new OffscreenCanvas(2*hyp, 2*adj);
 
 let lastTimestamp = 0;
 let angle = 0;
@@ -28,19 +31,14 @@ let angle = 0;
 function redraw(timestamp) {
 
     if (lastTimestamp === 0) lastTimestamp = timestamp;
-    const frameLength = (timestamp - lastTimestamp) / 700;
+    const frameLength = (timestamp - lastTimestamp) / 1000;
     lastTimestamp = timestamp;
-
     angle += frameLength/2;
+
     renderTriangle();
     renderHexagon();
-
-    const canvas = document.getElementById('rotateyCanvas');
-    const context = canvas.getContext('2d');
-
-
-    context.drawImage(hexagonCanvas, 0, 0);
     renderKaleidoscope();
+
     window.requestAnimationFrame(redraw);
 
 }
@@ -52,7 +50,7 @@ function renderTriangle() {
     context.clearRect(0,0,opp,adj);
 
     context.fillStyle = 'white';
-    context.globalCompositeOperation="source-in";
+    context.globalCompositeOperation="source-over";
 
     context.beginPath();
     context.moveTo(0, 0);
@@ -61,8 +59,7 @@ function renderTriangle() {
     context.lineTo(0, 0);
     context.fill();
 
-    context.globalCompositeOperation="source-out";
-
+    context.globalCompositeOperation="source-in";
     context.save();
     context.translate(opp/2, adj/2)
     context.rotate(angle);
@@ -70,49 +67,44 @@ function renderTriangle() {
     context.restore();
 
 }
+
 function renderHexagon() {
 
     const context = hexagonCanvas.getContext('2d');
 
     context.save();
-
     context.translate(hyp,adj);
-
-    for (let j = 0; j < 12; j ++) {
-
-        context.drawImage(triangleCanvas,0,0);
-
-        context.scale(1,-1);
-        context.drawImage(triangleCanvas,0,0);
-        context.scale(1,-1);
-
+    for (let j = 0; j < 6; j ++) {
         context.rotate(Math.PI/3);
-
+        context.drawImage(triangleCanvas,0,0);
+        context.scale(-1,1);
+        context.drawImage(triangleCanvas,0,0);
+        context.scale(-1,1);
     }
-
     context.restore();
 
 }
 
 function renderKaleidoscope() {
 
-    const canvas = document.getElementById('rotateyCanvas');
+    const canvas = document.getElementById('kaleidoscopeCanvas');
     const context = canvas.getContext('2d');
-
+    i=i+0.0002;
     context.fillStyle = 'black';
     context.fillRect(0,0,w,h);
 
     context.save();
-    context.translate(w/2-hyp, h/2-adj);
+    context.translate(w/2-hyp+i, h/2-adj);
 
     context.globalAlpha = 1;
     context.drawImage(hexagonCanvas, 0,0);
 
     context.globalAlpha = 0.5;
-    for (let i = 0; i < 6; i++) {
-        context.drawImage(hexagonCanvas, 2*adj*Math.sin(i*Math.PI/3),2*adj*Math.cos(i*Math.PI/3));
+      for (let i = 0; i < 24; i++) {
+        context.drawImage(hexagonCanvas, 2*adj*Math.sin(i*Math.PI/3), 2*adj*Math.cos(i*Math.PI/3));
+        context.drawImage(hexagonCanvas, 4*adj*Math.sin(i*Math.PI/6), 4*adj*Math.cos(i*Math.PI/6));
+        context.drawImage(hexagonCanvas, 6*adj*Math.sin(i*Math.PI/9), 6*adj*Math.cos(i*Math.PI/9));
     }
+        context.restore();
 
-    context.restore();
-
-}
+  }
